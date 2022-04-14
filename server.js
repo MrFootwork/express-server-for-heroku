0efc067fdb8b141d73bd9afeacbe14d692a2b93d
Mr Footwork <pandau.ting@gmail.com>
2022-04-14 18:40:30 +0200

    trying out automatic deploy on heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-	res.send('i changed this!!!')
+	res.send('i changed this again!!!!')
 })
 
 app.listen(process.env.PORT || 5000)
